e6062b2859b497ab1e7983fc249d9cfe16e17d52
Shadowking912 <96904882+Shadowking912@users.noreply.github.com>
2024-10-29 22:12:08 +0530

    added sampling

---
+++ sampling.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+def random_sampling(df, column_name, num_samples):
+    return df[column_name].sample(n = num_samples, replace=False)
+
+def monte_carlo_sampling(df, column_name, sample_size, num_samples):
+    samples = []
+    for i in range(num_samples):
+        l = df[column_name].sample(n = sample_size, replace=True)
+        samples.append(l.mean())
+
+    return samples
+
+
+#propotional sampling
+def stratified_sampling(df, column_name, num_samples):
+    return df.groupby(column_name, group_keys=False).apply(lambda x: x.sample(n = num_samples, replace=False))
